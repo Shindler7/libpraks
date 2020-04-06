2e145630437f3b2d79ffa25b91352ad7627f7c39
schindler7 <barmichev@gmail.com>
2020-04-06 23:02:08 +0300

    +upd lib

--- app/_dblib.py
+++ app/_dblib.py
@@ -35,7 +35,8 @@ urlbase = {
 30: ['Документация Flask (русский перевод)', 'https://flask-russian-docs.readthedocs.io/ru/latest/quickstart.html#quickstart-deployment', 'ru', 'site', ['python', 'flask', 'фласк']],
 31: ['Учебник «A Byte of Python» (русский перевод)', 'https://wombat.org.ua/AByteOfPython/toc.html#', 'ru', 'site', ['python']],
 32: ['Пишем ботов для Telegram на языке Python', 'https://groosha.gitbook.io/telegram-bot-lessons/', 'ru', 'site', ['python', 'telegram', 'bot', 'телеграм-бот', 'бот']],
-33: ['Про Git (автор: Скотт Шакон)', 'https://git-scm.com/book/ru/v2', 'ru', 'ebook', ['git']]
+33: ['Про Git (автор: Скотт Шакон)', 'https://git-scm.com/book/ru/v2', 'ru', 'ebook', ['git']],
+34: ['Руководство Django Girls', 'https://tutorial.djangogirls.org/ru/', 'ru', 'site', ['django, python']]
 
 }
 
